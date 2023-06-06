919470101c8bb34b6b92c353c4a274be3b5a7efd
Toktarla <111569015+Toktarla@users.noreply.github.com>
2023-06-06 14:51:24 +0300

    Implemented Rectangle class

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 class Rectangle
 {
+private:
+    int width;
+    int height;
 
+public:
+    Rectangle(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    int getWidth()
+    {
+        return width;
+    }
+    int getHeight()
+    {
+        return height;
+    }
+    void setWidth(int w)
+    {
+        width = w;
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
+    int calculateArea()
+    {
+        return width * height;
+    }
+    void printRectangle()
+    {
+        std::cout << "Width: " << width << std::endl;
+        std::cout << "Height: " << height << std::endl;
+    }
 };
 int main()
 {
-  
+    Rectangle rectangle(5, 10);
+    rectangle.printRectangle();
+    int area = rectangle.calculateArea();
+    std::cout << "Area: " << area << std::endl;
+    rectangle.setWidth(8);
+    rectangle.setHeight(6);
+    rectangle.printRectangle();
     return 0;
 }
